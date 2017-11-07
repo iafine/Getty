feb6605ba304941e78844e50ac025a1c2b59675d
castial <897257524@qq.com>
2017-11-07 15:59:10 +0800

    优化日期选择

--- Fleshy/Components/HYAlertView/HYDatePickView/HYDatePickerView.m
+++ Fleshy/Components/HYAlertView/HYDatePickView/HYDatePickerView.m
@@ -8,13 +8,15 @@
 
 #import "HYDatePickerView.h"
 
-@interface HYDatePickerView ()<UIPickerViewDelegate, UIPickerViewDataSource>
+#define kHYContentWidth kScreenWidth - 60    // 内容视图宽度
+#define kHYContentHeight 260   // 内容视图高度
+
+@interface HYDatePickerView ()
 
 @property (nonatomic, strong) UIView *contentView;   // 内容视图 (下面所有的控件都放在此处)
-@property (nonatomic, strong) UIView *toolView; // 工具栏视图
-@property (nonatomic, strong) UIButton *cancelBtn;  // 取消按钮
+@property (nonatomic, strong) UILabel *titleLabel;  // 标题
 @property (nonatomic, strong) UIButton *okBtn;      // 确定按钮
-@property (nonatomic, strong) UIPickerView *pickerView;     // 选择控件
+@property (nonatomic, strong) UIDatePicker *pickerView;     // 选择控件
 
 @end
 
@@ -26,10 +28,11 @@
         // 初始化基础控件
         self.backgroundColor = [UIColor colorWithRed:0 green:0 blue:0 alpha:0];
         [self addSubview:self.contentView];
-        [self.contentView addSubview:self.toolView];
+        [self.contentView addSubview:self.titleLabel];
         [self.contentView addSubview:self.pickerView];
-        [self.toolView addSubview:self.cancelBtn];
-        [self.toolView addSubview:self.okBtn];
+        [self.contentView addSubview:self.okBtn];
+        
+        self.contentView.center = self.center;
     }
     return self;
 }
@@ -37,36 +40,22 @@
 #pragma mark - Public Methods
 - (void)show {
     [[[UIApplication sharedApplication] keyWindow] addSubview:self];
-    [UIView animateWithDuration:0.3 animations:^{
-        CGRect newFrame = CGRectMake(0, CGRectGetMinY(self.contentView.frame) - CGRectGetHeight(self.contentView.frame), CGRectGetWidth(self.contentView.frame), CGRectGetHeight(self.contentView.frame));
-        self.contentView.frame = newFrame;
-        
+    self.contentView.transform = CGAffineTransformMakeScale(0.01, 0.01);
+    [UIView animateWithDuration:0.3 delay:0 options:UIViewAnimationOptionCurveEaseOut animations:^{
+        self.contentView.transform = CGAffineTransformIdentity;
         self.backgroundColor = [UIColor colorWithRed:0 green:0 blue:0 alpha:0.3];
+    } completion:^(BOOL finished){
+        // do something once the animation finishes, put it here
     }];
 }
 
 - (void)dismiss {
-    [UIView animateWithDuration:0.3 animations:^{
-        CGRect newFrame = CGRectMake(0, CGRectGetMinY(self.contentView.frame) + CGRectGetHeight(self.contentView.frame), CGRectGetWidth(self.contentView.frame), CGRectGetHeight(self.contentView.frame));
-        self.contentView.frame = newFrame;
-        
-        self.backgroundColor = [UIColor colorWithRed:0 green:0 blue:0 alpha:0];
-    } completion:^(BOOL finished) {
+    self.contentView.transform = CGAffineTransformIdentity;
+    [UIView animateWithDuration:0.3 delay:0 options:UIViewAnimationOptionCurveEaseOut animations:^{
+        self.contentView.transform = CGAffineTransformMakeScale(0.01, 0.01);
+    } completion:^(BOOL finished){
         [self removeFromSuperview];
     }];
-}
-
-#pragma mark - UIPickerViewDelegate
-- (NSInteger)pickerView:(UIPickerView *)pickerView numberOfRowsInComponent:(NSInteger)component {
-    return 10;
-}
-
-- (NSInteger)numberOfComponentsInPickerView:(UIPickerView *)pickerView {
-    return 2;
-}
-
-- (NSString *)pickerView:(UIPickerView *)pickerView titleForRow:(NSInteger)row forComponent:(NSInteger)component {
-    return [NSString stringWithFormat:@"测试%ld", row];
 }
 
 #pragma mark - Events
@@ -78,49 +67,55 @@
     [self dismiss];
 }
 
+- (void)pickerViewValueChange {
+    //创建一个日期格式
+    NSDateFormatter *fmt = [[NSDateFormatter alloc] init];
+    //设置日期的显示格式
+    fmt.dateFormat = @"hh-mm-ss";
+    //将日期转为指定格式显示
+    NSString *dateStr = [fmt stringFromDate:self.pickerView.date];
+    self.titleLabel.text = dateStr;
+}
+
 #pragma mark - Setter and Getter
 - (UIView *)contentView {
     if (!_contentView) {
-        _contentView = [[UIView alloc] initWithFrame:CGRectMake(0, kScreenHeight, kScreenWidth, 260)];
+        _contentView = [[UIView alloc] initWithFrame:CGRectMake(0, 0, kHYContentWidth, kHYContentHeight)];
         _contentView.backgroundColor = [UIColor whiteColor];
     }
     return _contentView;
 }
 
-- (UIView *)toolView {
-    if (!_toolView) {
-        _toolView = [[UIView alloc] initWithFrame:CGRectMake(0, 0, kScreenWidth, 40)];
+- (UILabel *)titleLabel {
+    if (!_titleLabel) {
+        _titleLabel = [[UILabel alloc] initWithFrame:CGRectMake(0, 0, kHYContentWidth, 40)];
+        _titleLabel.textAlignment = NSTextAlignmentCenter;
+        _titleLabel.backgroundColor = kMainColor;
+        _titleLabel.textColor = [UIColor whiteColor];
     }
-    return _toolView;
+    return _titleLabel;
 }
 
-- (UIButton *)cancelBtn {
-    if (!_cancelBtn) {
-        _cancelBtn = [UIButton buttonWithType:UIButtonTypeSystem];
-        _cancelBtn.frame = CGRectMake(0, 0, 60, 40);
-        [_cancelBtn setTitle:@"取消" forState:UIControlStateNormal];
-        [_cancelBtn addTarget:self action:@selector(clickedCancelBtnHandler) forControlEvents:UIControlEventTouchUpInside];
+- (UIDatePicker *)pickerView {
+    if (!_pickerView) {
+        _pickerView = [[UIDatePicker alloc] initWithFrame:CGRectMake(0, 60, kHYContentWidth, kHYContentHeight - 40 - 40)];
+        _pickerView.locale = [NSLocale localeWithLocaleIdentifier:@"zh"];
+        _pickerView.datePickerMode = UIDatePickerModeTime;
+        [_pickerView addTarget:self action:@selector(pickerViewValueChange) forControlEvents:UIControlEventValueChanged];
     }
-    return _cancelBtn;
+    return _pickerView;
 }
 
 - (UIButton *)okBtn {
     if (!_okBtn) {
-        _okBtn = [UIButton buttonWithType:UIButtonTypeSystem];
-        _okBtn.frame = CGRectMake(kScreenWidth - 60, 0, 60, 40);
+        _okBtn = [UIButton buttonWithType:UIButtonTypeCustom];
+        _okBtn.frame = CGRectMake(0, kHYContentHeight - 40, kHYContentWidth, 40);
         [_okBtn setTitle:@"确定" forState:UIControlStateNormal];
+        [_okBtn setTitleColor:[UIColor whiteColor] forState:UIControlStateNormal];
+        _okBtn.backgroundColor = kMainColor;
         [_okBtn addTarget:self action:@selector(clickedOKBtnHandler) forControlEvents:UIControlEventTouchUpInside];
     }
     return _okBtn;
-}
-
-- (UIPickerView *)pickerView {
-    if (!_pickerView) {
-        _pickerView = [[UIPickerView alloc] initWithFrame:CGRectMake(0, 60, kScreenWidth, 260 - 40)];
-        _pickerView.delegate = self;
-        _pickerView.dataSource = self;
-    }
-    return _pickerView;
 }
 
 @end

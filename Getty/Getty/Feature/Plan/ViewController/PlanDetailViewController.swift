fe3d389d44570c87998b7b77d94de2afb66ca584
castial <897257524@qq.com>
2017-12-17 19:24:31 +0800

    添加JTAppleCalendar

---
+++ Getty/Getty/Feature/Plan/ViewController/PlanDetailViewController.swift
@@ -0,0 +1,20 @@
+//
+//  PlanDetailViewController.swift
+//  Getty
+//
+//  Created by Hyyy on 2017/12/17.
+//  Copyright © 2017年 Getty. All rights reserved.
+//
+
+import UIKit
+import JTAppleCalendar
+
+class PlanDetailViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+    
+    
+}
